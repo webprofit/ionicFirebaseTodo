b662b51d58b1a7b9b36bbb4c1926a2cec88b4a69
Vitaliy Moskvita <vitaliymoskvita@Vitaliys-Mac.local>
2019-04-19 06:01:58 -0700

    first push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAQ-Q5AW-bMAE3okxjtJeCVcz2UIj9IOeI',
+    authDomain: 'testfirebase-84127.firebaseapp.com',
+    databaseURL: 'https://testfirebase-84127.firebaseio.com',
+    projectId: 'testfirebase-84127',
+    storageBucket: 'testfirebase-84127.appspot.com',
+    messagingSenderId: '455698471485'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAQ-Q5AW-bMAE3okxjtJeCVcz2UIj9IOeI',
+    authDomain: 'testfirebase-84127.firebaseapp.com',
+    databaseURL: 'https://testfirebase-84127.firebaseio.com',
+    projectId: 'testfirebase-84127',
+    storageBucket: 'testfirebase-84127.appspot.com',
+    messagingSenderId: '455698471485'
+  }
 };
 
 /*
